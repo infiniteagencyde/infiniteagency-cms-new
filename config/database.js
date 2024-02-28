952eaa56bcdcc5ff9b1b237a0aa287439d9d24e5
forthefuture05 <patrik25052005@gmail.com>
2024-02-28 20:48:28 +0100

    Updated Database

--- config/database.js
+++ config/database.js
@@ -1,9 +1,21 @@
-module.exports = ({ env }) => ({
+// module.exports = ({ env }) => ({
+//     connection: {
+//         client: 'postgres',
+//         connection: {
+//             connectionString: env('DATABASE_PRIVATE_URL')
+//         },
+//         pool: { min: 0 }
+//     }
+// });
+
+const path = require("path");
+
+module.exports = () => ({
+  connection: {
+    client: "sqlite",
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_PRIVATE_URL')
-        },
-        pool: { min: 0 }
-    }
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
+    },
+    useNullAsDefault: true,
+  },
 });
